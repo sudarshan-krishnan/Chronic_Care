76933fb6fc824412ab5809c07eae4a70f52e66c8
Sudarshan <k.sudarshan2904@gmail.com>
2024-05-11 01:51:24 +0530

    ran the model in vs

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pickle
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-# # loading 3 saved model
+# loading 3 saved model
 
 # diabetes_model = pickle.load(open('/Users/sudarshan/Documents/Code/Disease_Predictor/saved_models/diabetes_model.sav' , 'wb'))
 
@@ -21,18 +21,37 @@ with st.sidebar:
     
 # Diabetes Prediction Page
 if selected == 'Diabetes Prediction':
-   
+
     # page title
     st.title('Diabetes Prediction using ML')
 
-    Pregnancies = st.text_input('Number of Pregnancies')
-    Glucose = st.text_input('Glucose Level')
-    BloodPressure = st.text_input('Blood Pressure value')
-    SkinThickness = st.text_input('Skin Thickness value')
-    Insulin = st.text_input('Insulin Level')
-    BMI = st.text_input('BMI value')
-    DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function value')
-    Age = st.text_input('Age of the Person')
+    # getting the input data from the user
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        Pregnancies = st.text_input('Number of Pregnancies')
+
+    with col2:
+        Glucose = st.text_input('Glucose Level')
+
+    with col3:
+        BloodPressure = st.text_input('Blood Pressure value')
+
+    with col1:
+        SkinThickness = st.text_input('Skin Thickness value')
+
+    with col2:
+        Insulin = st.text_input('Insulin Level')
+
+    with col3:
+        BMI = st.text_input('BMI value')
+
+    with col1:
+        DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function value')
+
+    with col2:
+        Age = st.text_input('Age of the Person')
+
 
     # code for Prediction
     diab_diagnosis = ''
@@ -54,6 +73,7 @@ if selected == 'Diabetes Prediction':
             diab_diagnosis = 'The person is not diabetic'
 
     st.success(diab_diagnosis)
+
 
 
 if selected == 'Heart Disease Prediction':
